778c90c2a9393954abc1dfb7786480c63720cde2
ZaighamCodes <mdzaigham27@gmail.com>
2026-09-15 11:16:22 +0530

    fixes: fixed cors issue

--- src/main/java/com/savemark/app/config/SecurityConfig.java
+++ src/main/java/com/savemark/app/config/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -44,10 +49,57 @@ public class SecurityConfig{
         return new ProviderManager(authenticationProvider);
     }
 
+
+
+    // CORS Configuration
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://savemark-sooty.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "PATCH",
+                "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of(
+                "Authorization",
+                "Content-Type",
+                "Accept"
+        ));
+
+        // Required because you are using session-based authentication
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
     {
-        http.csrf(customizer->customizer.disable())
+        http
+                // Enable CORS
+                .cors(cors -> cors.configurationSource(
+                        corsConfigurationSource()
+                ))
+                .csrf(customizer->customizer.disable())
                 .authorizeHttpRequests(req->req
                         .requestMatchers("/register","/login").permitAll()
                         .anyRequest().authenticated())
